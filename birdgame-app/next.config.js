8b383c99d9da145557ca5669ac14472c6b942bc1
lsspkk <lsspkk@gmail.com>
2021-10-10 19:44:38 +0300

    fixing images

--- birdgame-app/next.config.js
+++ birdgame-app/next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   basePath: '/lintu',
   images: {
-    domains: ['localhost', 'xn--hyty-6qa.net'],
+    domains: [
+      'localhost',
+      'xn--hyty-6qa.net',
+      'www.xn--hyty-6qa.net',
+      'hyöty.net',
+      'www.hyöty.net',
+    ],
   },
 }
