ccb61c9370933b013dae5736f29c4ba48ec37304
lsspkk <lsspkk@gmail.com>
2021-10-17 19:55:20 +0300

    first audio level version

--- birdgame-app/components/AudioLevelButton.tsx
+++ birdgame-app/components/AudioLevelButton.tsx
@@ -1,44 +1,46 @@
 import { useRouter } from 'next/dist/client/router'
 import React, { ReactElement, useContext } from 'react'
 import { GameContext, GameContextInterface } from './state'
-import { BirdIconNoSound } from './Icons'
+import { BirdIcon } from './Icons'
 import { useStars } from './StarCircle'
 
-interface ImageLevelButtonProps {
+interface AudioLevelButtonProps {
   level: string
   onClick?: () => void
 }
-export function ImageLevelButton({
+export function AudioLevelButton({
   level,
-}: ImageLevelButtonProps): ReactElement {
+}: AudioLevelButtonProps): ReactElement {
   const { setBirdKnowledge, score }: GameContextInterface =
     useContext(GameContext)
   const router = useRouter()
+  // TODO fix the data model, save scores for image and audio separately
   const stars = useStars(score, Number(level))
   function selectLevel() {
     // store answers of this game
     setBirdKnowledge([])
-    router.push(`/imagelevel/${level}`)
+    router.push(`/audiolevel/${level}`)
   }
   return (
     <div
-      className={`w-1/2 bg-pink-${
+      className={`w-1/2 bg-indigo-${
         100 * (Number(level) + 1)
       } px-1 py-4 border-4`}
     >
       <div onClick={selectLevel} className="flex items-center justify-center">
         <div className="absolute -ml-28">
-          <BirdIconNoSound />
+          <BirdIcon />
         </div>
         <div className="opacity-0 pr-2">1</div>
-        {stars !== undefined && stars}
+        <div className="w-2 h-20"></div>
+        {/* {stars !== undefined && stars} */}
         <div className="text-white font-bold text-2xl pl-2">{level}</div>
       </div>
-      {/* <span className="bg-pink-200">.</span>
-      <span className="bg-pink-300">.</span>
-      <span className="bg-pink-400">.</span>
-      <span className="bg-pink-500">.</span>
-      <span className="bg-pink-600">.</span> */}
+      {/* <span className="bg-indigo-200">.</span>
+      <span className="bg-indigo-300">.</span>
+      <span className="bg-indigo-400">.</span>
+      <span className="bg-indigo-500">.</span>
+      <span className="bg-indigo-600">.</span> */}
     </div>
   )
 }
--- birdgame-app/pages/index.tsx
+++ birdgame-app/pages/index.tsx
@@ -2,13 +2,14 @@ import React, { ReactElement } from 'react'
 import { Layout } from '../components/Layout'
 import { UserSelectionController } from '../components/UserSelection'
 import { ImageLevelButton } from '../components/ImageLevelButton'
+import { AudioLevelButton } from '../components/AudioLevelButton'
 
 export default function Home(): ReactElement {
   return (
     <Layout>
       <UserSelectionController />
 
-      <p className="mt-3 text-2xl">Valitse tehtävä</p>
+      <p className="mt-3 text-2xl">Kuvan tunnistus</p>
 
       <div className="flex justify-center text-center flex-wrap">
         <ImageLevelButton level="1" />
@@ -16,6 +17,16 @@ export default function Home(): ReactElement {
         <ImageLevelButton level="3" />
         <ImageLevelButton level="4" />
         <ImageLevelButton level="5" />
+      </div>
+
+      <p className="mt-12 text-2xl">Äänen tunnistus</p>
+
+      <div className="flex justify-center text-center flex-wrap">
+        <AudioLevelButton level="1" />
+        <AudioLevelButton level="2" />
+        <AudioLevelButton level="3" />
+        <AudioLevelButton level="4" />
+        <AudioLevelButton level="5" />
       </div>
     </Layout>
   )
